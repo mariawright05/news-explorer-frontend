b2a14bdd3275565e364097e84d821dc393f2ed07
Maria Wright <maria@mariawrightstudios.com>
2021-05-03 14:24:27 -0400

    fix nested ul's

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,26 +8,30 @@ const Footer = () => {
     <div className="footer">
       <p className="footer__copyright">&#169; 2021 Maria Wright, Powered by News API</p>
       <ul className="footer__nav">
-        <div className="footer__nav-text-wrapper">
-          <li className="footer__nav-text">
-            <Link to="/" className="footer__nav-link">Home</Link>
-          </li>
-          <li className="footer__nav-text">
-            <a href="https://practicum.yandex.com/web/" target="_blank" rel="noreferrer" className="footer__nav-link">Practicum by Yandex</a>
-          </li>
-        </div>
-        <div className="footer__nav-icon-wrapper">
-          <li>
-            <a href="https://github.com/mariawright05" rel="noreferrer" target="_blank" className="footer__nav-icon">
-              <FaGithub />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/mariawright05/" rel="noreferrer" target="_blank" className="footer__nav-icon">
-              <FaLinkedin />
-            </a>
-          </li>
-        </div>
+        <li className="footer__nav-text-wrapper">
+          <ul>
+            <li className="footer__nav-text">
+              <Link to="/" className="footer__nav-link">Home</Link>
+            </li>
+            <li className="footer__nav-text">
+              <a href="https://practicum.yandex.com/web/" target="_blank" rel="noreferrer" className="footer__nav-link">Practicum by Yandex</a>
+            </li>
+          </ul>
+        </li>
+        <li className="footer__nav-icon-wrapper">
+          <ul>
+            <li>
+              <a href="https://github.com/mariawright05" rel="noreferrer" target="_blank" className="footer__nav-icon">
+                <FaGithub />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/mariawright05/" rel="noreferrer" target="_blank" className="footer__nav-icon">
+                <FaLinkedin />
+              </a>
+            </li>
+          </ul>
+        </li>
       </ul>
     </div>
   );
--- src/components/Sidebar/Sidebar.js
+++
@@ -1,7 +0,0 @@
-// import React from 'react';
-
-const Sidebar = () => {
-  return (console.log('sidebar'));
-};
-
-export default Sidebar;
